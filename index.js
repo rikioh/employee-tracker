fa4654400131a0c72ada8d6d15930568b1f785c8
Rikio Campbell <rikiocamp@gmail.com>
2021-03-18 16:42:28 -0700

    added all employees search and employee search by role id functions/inquirers

--- index.js
+++ index.js
@@ -2,10 +2,30 @@ const inquirer = require('inquirer');
 const mysql = require('mysql');
 const sequelize = require('./config/connection');
 
-sequelize.sync().then(() => {
-    console.log('Now listening')
-    inquirerSearch()
+// sequelize.sync().then(() => {
+//     console.log('Now listening')
+//     inquirerSearch()
+//   });
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: 'root',
+  
+    // Be sure to update with your own MySQL password!
+    password: '',
+    database: 'employees_db',
   });
+
+connection.connect((err) => {
+if (err) throw err;
+inquirerSearch();
+});
+  
 
 const inquirerSearch = () => {
     inquirer
@@ -14,16 +34,16 @@ const inquirerSearch = () => {
             type: 'rawlist',
             message: 'What would you like to do?',
             choices: [
-              'Find employee by id',
-              'Find role by id'
+              'View all employees',
+              'View all employees by role'
             ],
         })
         .then((answer) => {
             switch (answer.action){
-                case 'Find employee by id':
+                case 'View all employees':
                     employeeIDsearch()
                     break;
-                case 'Find role by id':
+                case 'View all employees by role':
                     roleIDsearch()
                     break
                 default:
@@ -35,8 +55,24 @@ const inquirerSearch = () => {
 
 const employeeIDsearch = () => {
     console.log("employee id search")
+    const query = 'SELECT * FROM employee';
+      connection.query(query, (err, res) => {
+        console.log(res)
+        });
 }
 
 const roleIDsearch = () => {
-    console.log("role id search")
-}
+    inquirer
+        .prompt({
+            name: 'role',
+            type: 'input',
+            message: 'What role would you like to search for?',
+        })
+        .then((answer) => {
+            console.log("role id search")
+            const query = 'SELECT * FROM employee WHERE ?';
+            connection.query(query, { role_id: answer.role }, (err, res) => {
+            console.log(res)
+        });
+        })
+    }
