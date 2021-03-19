9a830f6d3f70a9773c62f97d98d61d9fcf4eca9d
Rikio Campbell <rikiocamp@gmail.com>
2021-03-18 17:31:26 -0700

    completed all view prompts/sql queries from the initial inquirer screen

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql');
 const sequelize = require('./config/connection');
+const cTable = require('console.table');
 
 // sequelize.sync().then(() => {
 //     console.log('Now listening')
@@ -35,7 +36,8 @@ const inquirerSearch = () => {
             message: 'What would you like to do?',
             choices: [
               'View all employees',
-              'View all employees by role'
+              'View all employees by role',
+              'View all employees by department'
             ],
         })
         .then((answer) => {
@@ -45,6 +47,9 @@ const inquirerSearch = () => {
                     break;
                 case 'View all employees by role':
                     roleIDsearch()
+                    break
+                case 'View all employees by department':
+                    departmentIDsearch()
                     break
                 default:
                     console.log(`Invalid action: ${answer.action}`);
@@ -57,7 +62,7 @@ const employeeIDsearch = () => {
     console.log("employee id search")
     const query = 'SELECT * FROM employee';
       connection.query(query, (err, res) => {
-        console.log(res)
+        console.table(res)
         });
 }
 
@@ -72,7 +77,44 @@ const roleIDsearch = () => {
             console.log("role id search")
             const query = 'SELECT * FROM employee WHERE ?';
             connection.query(query, { role_id: answer.role }, (err, res) => {
-            console.log(res)
+            console.table(res)
         });
+    })
+}
+
+const departmentIDsearch = () => {
+    inquirer
+        .prompt({
+            name: 'department',
+            type: 'input',
+            message: 'What department would you like to search for?',
         })
-    }
+        .then((answer) => {
+            console.log("department id search")
+            // empty array to be filled with all possible role Id's in the department
+            const roles = []
+            // begin running department query to find roles
+            const query1 = 'SELECT * FROM role WHERE ?';
+            connection.query(query1, { department_id: answer.department }, (err, res) => {
+                // pulls all role_ids from the department chosen
+                for(i=0;i<res.length;i++){
+                    roles.push(res[i].role_id)
+                }
+                // begin running query for selecting all employees
+                const query2 = 'SELECT * FROM employee WHERE ?';
+                const depEmployee = []
+                var count = 0
+                for(i=0;i<roles.length;i++){
+                    connection.query(query2, { role_id: roles[i] }, (err, res) => {
+                    // pulls all employees from the roles found
+                    depEmployee.push(res[0])
+                    count++
+                    if(count==roles.length){
+                        console.table(depEmployee)
+                    }
+                })
+
+            }
+            });
+        })
+}
